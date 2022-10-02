42c0be7906f54dfc238e94a1bb21357d59be4ec2
Ruan Bekker <ruan.ru.bekker@gmail.com>
2022-10-03 01:04:51 +0200

    Create main.tf

---
+++ terraform/aws/modules/vpc/example/main.tf
@@ -0,0 +1,10 @@
+module "vpc" {
+  source                  = "../../vpc"
+
+  region               = var.region
+  environment          = var.environment
+  vpc_cidr             = var.vpc_cidr
+  public_subnets_cidr  = var.public_subnets_cidr
+  private_subnets_cidr = var.private_subnets_cidr
+
+}
