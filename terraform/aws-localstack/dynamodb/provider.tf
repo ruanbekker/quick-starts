ffbbd97f8221d52e99b9ce41e221edc84a6108b8
Ruan Bekker <ruan.ru.bekker@gmail.com>
2023-06-09 00:40:10 +0200

    Update provider.tf

--- terraform/aws-localstack/dynamodb/provider.tf
+++ terraform/aws-localstack/dynamodb/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.23.0"
+    }
+  }
+}
+
 provider "aws" {
   region                      = "us-east-1"
   access_key                  = "quickstart"
